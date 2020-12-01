f4dd2b1e170a905930aac92033462717a8254412
Sove67 <afseth598@gmail.com>
2020-11-30 19:58:27 -0800

    Author Dimensions Fix

--- scripts/logout.js
+++ scripts/logout.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // When search is clicked, navigate to the post list page with the search term in the url queries.
     $(".logOut").on("click", function () {
         firebase.auth().signOut().then(function () {
-            window.location.href = "/index.html";
+            window.location.href = "/home.html";
         }, function (error) {
             console.log("ERROR.");
         });
